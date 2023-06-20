27fe82945cdd58b3b9f04d2e1148e3311b9a4b2a
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2023-06-20 01:49:17 -0300

    inicializacion de proyecto

---
+++ ejemplos_platformio/adc_dual_channel_polling/src/main.c
@@ -0,0 +1,15 @@
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_adc.h"
+#include "stm32f1xx_hal_gpio.h"
+
+/**
+ * @brief Programa principa;
+ */
+int main(void) {
+
+    while(1) {
+
+    }
+
+    return 0;
+}
