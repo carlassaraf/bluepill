474289f0bc2dfd6ed39aef075a85a37ef82b48a5
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2023-06-19 23:19:25 -0300

    tareas de freertos

--- ejemplos_platformio/freertos_gpio_blinky/src/main.c
+++ ejemplos_platformio/freertos_gpio_blinky/src/main.c
@@ -3,16 +3,16 @@
 
 #include "clocks.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
 #define LED_GPIO    GPIOC           // Puerto del LED de la Bluepill
 #define LED_PIN     GPIO_PIN_13     // Pin del LED de la Bluepill
 
 /**
- * @brief Programa principal
+ * @brief Tarea de inicializacion 
  */
-int main(void) {
-    // Inicializo clocks del sistema y perifericos
-    SystemClock_Config();
-
+void task_init(void *params) {
     // Declaro estructura de configuracion de GPIO
     GPIO_InitTypeDef gpio_config = {
         .Pin = LED_PIN,
@@ -25,10 +25,55 @@ int main(void) {
     __HAL_RCC_GPIOC_CLK_ENABLE();
     // Habilito el GPIO del LED con la cofiguracion elegida
     HAL_GPIO_Init(LED_GPIO, &gpio_config);
+    // Elimino tarea
+    vTaskDelete(NULL);
+}
+
+/**
+ * @brief Tarea para conmutar el LED 
+ */
+void task_led_blinky(void *params) {
+    // Delay para bloquear la tarea
+    const uint16_t delay_ms = 500;
 
     while(1) {
-
+        // Conmuto LED
+        HAL_GPIO_TogglePin(LED_GPIO, LED_PIN);
+        // Bloqueo tarea
+        vTaskDelay(delay_ms);
     }
+}
 
+/**
+ * @brief Programa principal
+ */
+int main(void) {
+    // Inicializo clocks del sistema y perifericos
+    SystemClock_Config();
+
+    // Creo tareas
+
+    xTaskCreate(
+        task_init,
+        "Init",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        tskIDLE_PRIORITY + 2UL,
+        NULL
+    );
+
+    xTaskCreate(
+        task_led_blinky,
+        "LED",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        tskIDLE_PRIORITY + 1UL,
+        NULL
+    );
+
+    // Inicializo scheduler de FreeRTOS
+    vTaskStartScheduler();
+
+    while(1);
     return 0;
 }
